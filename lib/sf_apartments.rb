872dce2bb2fa9dac186790dc4be86a8b9f2c2902
Jamie English <jamienglish@gmail.com>
2017-03-02 19:33:22 -0800

    stuff n shit

--- lib/sf_apartments.rb
+++ lib/sf_apartments.rb
@@ -48,67 +48,126 @@ module SFApartments
     end
 
     def self.get_url(doc)
-      url = doc.at_xpath('//link[rel=canonical]/@href').value
-      java.net.URI.new(url)
+      url = doc.at_xpath('//link[@rel="canonical"]/@href').value
+      URI.parse(url)
     end
 
     def self.get_body(doc)
-      doc.at_xpath('//section[@id=postingbody]/text()').text
+      doc.xpath('//section[@id="postingbody"]/text()').text.strip.squeeze("\n")
     end
 
     def self.parse_page(path, doc)
-      { :"listing/url"          => get_url(doc),
-        # :"listing/doc"          => doc,
-        :"listing/body"         => get_body(doc),
-        :"listing/price"        => get_price(doc),
-        :"listing/move_in_date" => get_move_in_date(doc),
-        :"listing/bed_count"    => get_bed_count(doc),
-        :"listing/bath_count"   => get_bath(doc),
-        :"listing/tags"         => get_tags(doc), }
+      { :url.ns          => get_url(doc),
+        # :doc.ns          => doc,
+        :body.ns         => get_body(doc),
+        :price.ns        => get_price(doc),
+        :move_in_date.ns => get_move_in_date(doc),
+        :bed_count.ns    => get_bed_count(doc),
+        :bath_count.ns   => get_bath(doc),
+        :tags.ns         => get_tags(doc), }
     end
 
-    S.def(:"listing/id", S.with_gen(S.and(String, method(:Integer)), ->(r) {
+    S.def(:id.ns, S.with_gen(S.and(String, method(:Integer)), ->(r) {
       Gen.generate(S.gen(:natural_integer.ns(S))).to_s
     }))
 
-    S.def(:"listing/url", S.with_gen(java.net.URI, ->(r) { java.net.URI.new(Gen.generate(S.gen(URI)).to_s) }))
-    S.def(:"listing/price", S.int_in(1_000..10_000))
-    S.def(:"listing/move_in_date", S.date_in(Date.new(2017, 1, 1)..Date.new(2017, 6, 1)))
-    S.def(:"listing/bed_count", S.float_in(:min => 0.0, :max => 10.0, :infinite => false, :nan => false))
-    S.def(:"listing/bath_count", S.float_in(:min => 0.0, :max => 10.0, :infinite => false, :nan => false))
+    S.def(:url.ns, URI)
+    S.def(:price.ns, S.int_in(1_000..10_000))
+    S.def(:move_in_date.ns, S.date_in(Date.new(2017, 1, 1)..Date.new(2017, 6, 1)))
+    S.def(:bed_count.ns, S.float_in(:min => 0.0, :max => 10.0, :infinite => false, :nan => false))
+    S.def(:bath_count.ns, S.float_in(:min => 0.0, :max => 10.0, :infinite => false, :nan => false))
 
     example_tags = Set["apartment", "laundry in bldg", "carport", "cats are OK - purrr", "dogs are OK - wooof", "loft", "no smoking", "attached garage", "w/d in unit", "condo", "furnished", "detached garage", "off-street parking", "flat", "street parking", "no parking", "laundry on site", "wheelchair accessible"]
-    S.def(:"listing/tag", S.with_gen(String, S.gen(example_tags)))
-    S.def(:"listing/tags", S.coll_of(:"listing/tag", :kind => Set, :gen_max => 6))
-    S.def(:"#{SFApartments}/listing", S.keys(:req => [:"listing/url", :"listing/price", :"listing/move_in_date", :"listing/bed_count", :"listing/bath_count", :"listing/tags"]))
+    S.def(:tag.ns, S.with_gen(String, S.gen(example_tags)))
+    S.def(:tags.ns, S.coll_of(:tag.ns, :kind => Set, :gen_max => 6))
+    S.def(:listing.ns(SFApartments), S.keys(:req => [:url.ns, :price.ns, :move_in_date.ns, :bed_count.ns, :bath_count.ns, :tags.ns]))
   end
 
   module DB
     S = Speculation
     using S::NamespacedSymbols.refine(self)
 
-    S.def(:"db/listing", S.and(:"#{SFApartments}/listing", S.conformer { |listing|
-      persisted_keys = Set[:"listing/url", :"listing/price", :"listing/move_in_date", :"listing/bed_count", :"listing/bath_count", :"listing/tags"]
+    java_import "datomic.Peer"
+    java_import "datomic.Connection"
+    java_import "datomic.Util"
 
-      listing.
-        select { |k, v| persisted_keys.include?(k) }.
-        merge(:"listing/move_in_date" => listing.fetch(:"listing/move_in_date").to_time.to_java).
-        merge(:"listing/tags" => java.util.HashSet.new(listing.fetch(:"listing/tags").to_a)).
-        map { |k, v| [":#{k}", v] }.
-        to_h
-    }))
-
-    def self.write(conn, listings)
-      conformed_listings = listings.map { |listing|
-        # raise "invalid listing" unless S.valid?(:"db/listing", listing)
-        S.conform(:"db/listing", listing)
-      }
-
-      conn.transact(conformed_listings).get
+    def self.write_listings(conn, listings)
+      prepped = prep_for_datomic(listings)
+      conn.transact(prepped).get
     end
 
-    S.fdef method(:write),
-      :args => S.cat(:conn => Java::Datomic::Connection, :listings => S.coll_of(:"SFApartments/listing")),
+    def self.read_listings(conn)
+      results = Peer.q(<<-DAT, conn.db)
+        [:find (pull ?e [*])
+         :where [?e :listing/url ?]]
+      DAT
+
+      results.map(&:first).map(&method(:rubyify)).map { |listing|
+        listing.map { |k, v|
+          if k.namespace == "listing"
+            k = k.name.to_sym.ns(SFApartments::Listing)
+          end
+          [k, v]
+        }.to_h
+      }
+    end
+
+    S.fdef method(:write_listings),
+      :args => S.cat(:conn => Connection, :listings => S.coll_of(:listing.ns(SFApartments))),
       :ret  => Java::ClojureLang::PersistentArrayMap
+
+    def self.prep_for_datomic(listings)
+      persisted_keys = Set[:url.ns(Listing),
+                           :price.ns(Listing),
+                           :move_in_date.ns(Listing),
+                           :bed_count.ns(Listing),
+                           :bath_count.ns(Listing),
+                           :tags.ns(Listing),
+                           :body.ns(Listing)]
+
+      listings.map { |listing|
+        javaify(
+          listing.
+          select { |k, v| persisted_keys.include?(k) }.
+          map { |k, v| [match_db_namespace(k), v] }.
+          to_h
+        )
+      }
+    end
+
+    S.fdef method(:prep_for_datomic),
+      :args => S.cat(:listings => S.coll_of(:listing.ns(SFApartments))),
+      :ret => S.hash_of(String, :any.ns(S))
+
+    def self.match_db_namespace(k)
+      k.name.to_sym.ns("listing")
+    end
+
+    def self.look_like_clj_symbol(k)
+      ":#{k}"
+    end
+
+    def self.javaify(x)
+      case x
+      when Symbol then look_like_clj_symbol(x)
+      when Date   then x.to_time.to_java
+      when Set    then java.util.HashSet.new(x.to_a)
+      when URI    then java.net.URI.new(x.to_s)
+      when Hash   then Util.map(*x.map { |k, v| [javaify(k), javaify(v)] }.flatten)
+      when Array  then Util.list(*x.map { |x| javaify(x) }.to_a)
+      else x
+      end
+    end
+
+    def self.rubyify(x)
+      case x
+      when Java::ClojureLang::Keyword then S::NamespacedSymbols.symbol(x.namespace, x.name)
+      when Java::JavaUtil::Date       then Time.at(x.time / 1000).to_date
+      when Java::JavaUtil::Collection then x.map(&method(:rubyify))
+      when Java::JavaNet::URI         then URI(x.to_s)
+      when Java::JavaUtil::Map        then x.map { |k, v| [rubyify(k), rubyify(v)] }.to_h
+      else x
+      end
+    end
   end
 end
